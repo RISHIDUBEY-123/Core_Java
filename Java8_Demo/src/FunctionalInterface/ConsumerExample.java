9dd5c35e539dbd26b2e2bc351cd719b02b4d0635
rishi <rishi@rishi-Inspiron-3558>
2022-01-09 22:25:20 +0530

    Consumer Implementation.

--- Java8_Demo/src/FunctionalInterface/ConsumerExample.java
+++ Java8_Demo/src/FunctionalInterface/ConsumerExample.java
@@ -4,7 +4,7 @@ public class ConsumerExample {
 
 	public static void main(String[] args) {
 		
-		Consumer<String>  c1=(s) -> System.out.println(s);
+		Consumer<String> c1=(s) -> System.out.println(s);
         
 		c1.accept("java8");
 	}
